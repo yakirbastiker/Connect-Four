e863bfcc29500bc86e579ea52c865171e939358b
yakir <yakirbastiker@gmail.com>
2019-10-18 17:07:26 +0300

    check-winner

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     //drow a grid
-    const connect4 = new Connect4('#connect4')
+    const connect4 = new Connect4('#connect4');
+
+    connect4.onPlayerMove = function() {
+        $('#player').text(connect4.player)
+    };
+
+    $('#restart').click(function(){
+        connect4.restart();
+    });
 });
--- connect4.js
+++ connect4.js
@@ -4,14 +4,17 @@ class Connect4 {
         this.cols =7;
         this.player = 'red';
         this.selector = selector;
-
+        this.isGameOver = false;
+        this.onPlayerMove = function(){};
         this.createGrid();
         this.setupEventListeners();
     }
 
     createGrid() {
         const $board = $(this.selector);
-        
+        $board.empty();
+        this.isGameOver = false;
+        this.player = 'red';
         for(let row = 0; row < this.rows; row++) {
             const $row = $('<div>').addClass('row');
 
@@ -46,6 +49,9 @@ class Connect4 {
 
 
         $board.on('mouseenter', '.col.empty', function() {
+
+            if(that.isGameOver) return;
+
            // console.log('here', this);
             const col = $(this).data('col');
            // console.log(col)
@@ -61,6 +67,9 @@ class Connect4 {
         });
 
         $board.on('click','.col.empty', function(){
+
+            if(that.isGameOver) return;
+
             const col = $(this).data('col');
             const row = $(this).data('row');
 
@@ -68,9 +77,92 @@ class Connect4 {
             const $lastEmptyCell = findLastEmptyCell(col);
             $lastEmptyCell.removeClass(`empty next-${that.player}`);
             $lastEmptyCell.addClass(that.player);
+            $lastEmptyCell.data('player', that.player);
 
+
+            const winner = that.checkForWinner(
+                $lastEmptyCell.data('row'),
+                $lastEmptyCell.data('col')
+            );
+
+            if(winner) {
+                // do stuff
+                that.isGameOver = true;
+                alert(`Game Over! ${that.player}  has won`);
+                $('.col.empty').removeClass('empty');
+                return;
+            }
+
+            
             that.player = (that.player === 'red') ? 'black' : 'red';
+            that.onPlayerMove();
             $(this).trigger('mouseenter');
         });
     }
+
+
+    checkForWinner(row, col) {
+        const that = this;
+
+        function getCell(row,col) {
+            return $(`.col[data-row='${row}'][data-col='${col}']`);
+        }
+
+        function checkDirection(direction) {
+            let total = 0;
+            let i = row + direction.i;
+            let j =col + direction.j;
+            let $next = getCell(i, j);
+
+            while (i > 0 &&
+                   i < that.rows &&
+                   j >= 0 &&
+                   j< that.cols &&
+                   $next.data('player') === that.player ) {
+                       total++;
+                       i += direction.i;
+                       j += direction.j;
+                       $next = getCell(i, j);
+                   }
+
+                   return total;
+        }
+
+        function checkWin(directionA, directionB) {
+            const total = 1 + 
+            checkDirection(directionA) +
+            checkDirection(directionB);
+
+            if(total >=4) {
+                return that.player;
+            }else {
+                return null;
+            }
+        }
+
+        function checkDiagonalBLtoTR() {
+            return checkWin({i: 1, j:-1}, {i:1, j:1});
+        }
+
+        function checkDiagonaTLLtoBR() {
+            return checkWin({i: 1, j:1}, {i:-1, j:-1});
+        }
+
+        function checkVerticals(){
+            return checkWin({i: -1, j:0}, {i:1, j:0});
+        }
+
+
+        function checkHorizontals(){
+            return checkWin({i: 0, j:-1}, {i:0, j:1});
+        }
+
+        return checkVerticals() || checkHorizontals() || checkDiagonalBLtoTR() || checkDiagonaTLLtoBR()
+       }
+
+
+       restart() {
+           this.createGrid();
+           this.onPlayerMove();
+       }
 }
